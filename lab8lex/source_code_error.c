c5af25f33a2743e48d50743984cccef897bd2385
ghetinavlad <73642253+ghetinavlad@users.noreply.github.com>
2022-01-10 13:48:29 +0200

    Add files via upload

---
+++ lab8lex/source_code_error.c
@@ -0,0 +1,17 @@
+START
+  int a;
+  int b;
+  int &c;
+  int avg;
+  string str;
+  str = "23
+  read(a);
+  read(b);
+  read(c);
+  int sum;
+  int avg;
+  sum = a + b + d;
+  avg = sum / 3;
+
+  print(avg);
+EXIT
